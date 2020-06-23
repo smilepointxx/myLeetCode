763ec34a6e21a583009dcdec3d3e6d8ba93b54dd
xingxing.chang <xingxing.chang>
2020-06-24 00:01:43 +0800

    queue

--- src/main/java/com/leet/queue/code/NumIslands.java
+++ src/main/java/com/leet/queue/code/NumIslands.java
@@ -10,28 +10,54 @@ import java.util.concurrent.LinkedBlockingQueue;
 public class NumIslands {
     public int numIslands(char[][] grid) {
 
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
         int ans = 0;
         //wide
         int nw = grid.length;
         //length
         int nl = grid[0].length;
 
+        int max = Math.max(nw, nl);
+
+
         for (int i = 0; i < nw; i++) {
-            for (int j = nl; j < nl; j++) {
+            for (int j = 0; j < nl; j++) {
                 if (grid[i][j] == '1') {
+                    ans ++;
                     Queue<Integer> queue = new LinkedBlockingQueue<Integer>();
-                    queue.add(1);
+                    queue.add(i * max + j);
                     while (!queue.isEmpty()) {
+                        int value = queue.poll();
+                        int w = value / max;
+                        int l = value % max;
+
                         //up down left right
-                        if (i - 1 > 0 && grid[i - 1][j] == '1') {
-                            queue.offer()
+                        if (w - 1 > 0 && grid[w - 1][l] == '1') {
+                            queue.offer(max * (w - 1) + l);
+                            grid[w - 1][l] = '0';
+                        }
+
+                        if (w + 1 < nw && grid[w + 1][l] == '1') {
+                            queue.offer(max * (w - 1) + l);
+                            grid[w + 1][l] = '0';
+                        }
+
+                        if (l - 1 > 0 && grid[w][l - 1] == '1') {
+                            queue.offer(max * (w - 1) + l);
+                            grid[w][l - 1] = '0';
+                        }
+
+                        if (l + 1 < nl && grid[w][l + 1] == '1') {
+                            queue.offer(max * (w - 1) + l);
+                            grid[w][l + 1] = '0';
                         }
 
                     }
                 }
             }
         }
-
-        return 1;
+        return ans;
     }
 }
