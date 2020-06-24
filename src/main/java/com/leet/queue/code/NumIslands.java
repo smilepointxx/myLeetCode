9ba8f039d9605d60995d3f44e4b240cea796ebef
unknown <xingxing.chang@qunarservers.com>
2020-06-24 19:41:41 +0800

    ac

--- src/main/java/com/leet/queue/code/NumIslands.java
+++ src/main/java/com/leet/queue/code/NumIslands.java
@@ -8,7 +8,8 @@ import java.util.concurrent.LinkedBlockingQueue;
  * @Date: 2020/6/23 21:46
  */
 public class NumIslands {
-    public int numIslands(char[][] grid) {
+
+    public static int numIslands(char[][] grid) {
 
         if (grid == null || grid.length == 0) {
             return 0;
@@ -25,6 +26,7 @@ public class NumIslands {
         for (int i = 0; i < nw; i++) {
             for (int j = 0; j < nl; j++) {
                 if (grid[i][j] == '1') {
+                    grid[i][j] = '0';
                     ans ++;
                     Queue<Integer> queue = new LinkedBlockingQueue<Integer>();
                     queue.add(i * max + j);
@@ -33,24 +35,24 @@ public class NumIslands {
                         int w = value / max;
                         int l = value % max;
 
-                        //up down left right
-                        if (w - 1 > 0 && grid[w - 1][l] == '1') {
+                        //up
+                        if (w - 1 >= 0 && grid[w - 1][l] == '1') {
                             queue.offer(max * (w - 1) + l);
                             grid[w - 1][l] = '0';
                         }
-
+                        //down
                         if (w + 1 < nw && grid[w + 1][l] == '1') {
-                            queue.offer(max * (w - 1) + l);
+                            queue.offer(max * (w + 1) + l);
                             grid[w + 1][l] = '0';
                         }
-
-                        if (l - 1 > 0 && grid[w][l - 1] == '1') {
-                            queue.offer(max * (w - 1) + l);
+                        //left
+                        if (l - 1 >= 0 && grid[w][l - 1] == '1') {
+                            queue.offer(max * w + l - 1);
                             grid[w][l - 1] = '0';
                         }
-
+                        //right
                         if (l + 1 < nl && grid[w][l + 1] == '1') {
-                            queue.offer(max * (w - 1) + l);
+                            queue.offer(max * w + l + 1);
                             grid[w][l + 1] = '0';
                         }
 
@@ -59,5 +61,19 @@ public class NumIslands {
             }
         }
         return ans;
+    }
+
+    public static void main(String[] args) {
+        char[][] grid = new char[3][3];
+        char[] g1 = new char[]{'1', '1', '1'};
+        char[] g2 = new char[]{'0', '1', '0'};
+        char[] g3 = new char[]{'1', '1', '1', '0', '0'};
+
+        grid[0] = g1;
+        grid[1] = g2;
+        grid[2] = g3;
+
+        System.out.println(numIslands(grid));
+
     }
 }
